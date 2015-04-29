07bd98c252eaa9ce4a734eba839588924be66b6a
YOUR NAME <YOUR@EMAIL.com>
2015-04-29 16:31:02 +0530

    Initial Commit

---
+++ Retail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Retail
+//
+//  Created by Vijay Pratap Singh on 29/04/15.
+//  Copyright (c) 2015 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Retail/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Retail
+//
+//  Created by Vijay Pratap Singh on 29/04/15.
+//  Copyright (c) 2015 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
